b5ded3bdd18a985e16bcdcb3ce0ceab6b66c29c5
Gulmira <Gulmira@MIIRRAJ>
2023-12-24 16:00:36 +0500

    Add project files.

---
+++ Tyuiu.BardievaGA.Sprint1.Task0.V19.Lib/DataService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Важно! Следует к библиотеке кассов подключить файл tyuiu.cources.programming.interfaces.dll
+using tyuiu.cources.programming.interfaces.Sprint1;
+
+namespace Tyuiu.BardievaGA.Sprint1.Task0.V19.Lib
+{
+    public class DataService : ISprint1Task0V19
+    {
+        public double Calculate()
+        {
+            return 4 / 2 * 5 / (3 + 2) * 5;
+        }
+    }
+}
---
+++ Tyuiu.BardievaGA.Sprint1.Task0.V19.Test/DataServiseTest.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.BardievaGA.Sprint1.Task0.V19.Lib;
+
+namespace Tyuiu.BardievaGA.Sprint1.Task0.V19.Test
+{
+    [TestClass]
+    public class DataServiseTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            var res = ds.Calculate();
+            Assert.AreEqual(10, res);
+        }
+    }
+}
